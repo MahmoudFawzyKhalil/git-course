c248645e37242b354ed6629f25bf1521954c317a
Mahmoud Fawzy <mahmoudfawzykhalil98@gmail.com>
2022-02-10 11:50:15 +0200

    Modified main

--- main.java
+++ main.java
@@ -2,5 +2,14 @@ public class Main{
 	public static void main()
 	{
 		System.out.println("hello world!!");
+
+		System.out.println("Hello mahmoud");
+
+
+		System.out.println("Hello mahmoud");
+
+		System.out.println("Hello mahmoud");
+
+		System.out.println("Hello mahmoud");
 	}
 }
